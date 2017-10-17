c4a18c87e684a9348684ff8c2dfd408a7e6ee6a6
Sella Rafaeli <sella.rafaeli@gmail.com>
2017-10-17 11:40:10 +0300

    settings page

--- bl/users.rb
+++ bl/users.rb
@@ -1,6 +1,6 @@
 $users = $mongo.collection('users')
 
-USER_FIELDS = [:nick,:address]
+USER_FIELDS = [:nick,:name,:address]
 
 def set_users
   $users.delete_many
@@ -15,9 +15,22 @@ get '/login/:nick' do
   redirect '/'
 end
 
+
 get '/users/:nick' do 
   erb :'/users/profile', locals: {user: $users.get(nick: pr[:nick])}, layout: :layout
 end
+
+# settings
+get '/settings' do
+  erb :'/user_settings/settings_page', locals: {user: cu}, layout: :layout
+end
+
+post '/settings' do 
+  $users.update_id(cuid,pr)
+  flash.message = 'Updated!'
+  redirect back
+end
+# end settings
 
 get '/logout' do
   session.clear
