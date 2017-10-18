60a377a0b04c7d9760bcc18e70b327c0a11c0d37
Sella Rafaeli <sella.rafaeli@gmail.com>
2017-10-18 19:35:53 +0300

    auto-message.

---
+++ bl/orgs.rb
@@ -0,0 +1,17 @@
+$orgs = $mongo.collection('orgs')
+
+ORG_FIELDS = [:name, :type]
+
+def create_org(name)
+  $orgs.add(name: name)
+end
+
+get '/orgs/:id' do
+  org = $orgs.get(pr[:id]) || $orgs.random
+  erb :'/orgs/org_page', locals: {org: org}, layout: :layout
+end
+
+post '/org/:id' do
+  $orgs.update_id(pr[:id],pr.just(ORG_FIELDS))
+  flash_and_back('Thanks!')
+end
