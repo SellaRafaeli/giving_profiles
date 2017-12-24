cb938568fc4c659fb975707b97a42d27c16d3b48
Sella Rafaeli <sella.rafaeli@gmail.com>
2017-12-24 21:00:01 +0200

    fb posts in feed

--- bl/feed.rb
+++ bl/feed.rb
@@ -7,4 +7,10 @@ def my_network_donations
   donations = $donations.all(crit, opts)[0..100]
 end
 
+def my_feed_orgs
+  org_ids  = user_donations(cuid).mapo('org_id')
+  orgs     = $orgs.get_many(_id: {'$in': org_ids})
+  fb_pages = orgs.mapo('facebook_page').compact.uniq
+end
+
 get '/refresh' do :refresh end
