a5b0802e117ba260c7c8902d8f24f410af8f0a1c
Sella Rafaeli <sella.rafaeli@gmail.com>
2018-01-19 16:47:52 +0200

    privacy fixes

--- bl/feed.rb
+++ bl/feed.rb
@@ -2,9 +2,14 @@ def my_network_donations
   #users = $users.all(network: cu['network'])
   users = $users.all
 
-  crit = {user_id: {'$in': users.mapo('_id')}, private: {'$ne': 'true'}}
+  crit = {user_id: {'$in': users.mapo('_id')}}
   opts = {sort: [{created_at: -1}], limit: 1000}
   donations = $donations.all(crit, opts)[0..100]
+  
+  donations = donations.reject {|don| 
+    (don['user_id']!=cuid) && ((don['private'].to_s == 'true') || is_org_id_private(don['org_id'],don['user_id']))    
+  }
+  donations
 end
 
 def my_donated_orgs
