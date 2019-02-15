0ceb942ff2c81664a857bcec5894b58dc31d5934
Mark Donald Amobi <markdamobi@gmail.com>
2019-02-14 22:00:22 -0600

    remove unneeded uniqueness from donations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_142050) do
+ActiveRecord::Schema.define(version: 2019_02_15_025104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_02_06_142050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["organization_id"], name: "index_donations_on_organization_id"
-    t.index ["user_id", "organization_id"], name: "index_donations_on_user_id_and_organization_id", unique: true
+    t.index ["user_id", "organization_id"], name: "index_donations_on_user_id_and_organization_id"
     t.index ["user_id"], name: "index_donations_on_user_id"
   end
 
