9bd161d4292b1122dac8b89de0c34d316e5d9ef5
Brittney Braxton <brittneynbraxton@gmail.com>
2019-02-19 20:06:02 -0600

    Add Bootstrap gem, create GivingProfiles controller

---
+++ app/controllers/giving_profiles_controller.rb
@@ -0,0 +1,4 @@
+class GivingProfilesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'giving_profiles/index'
   get 'home/index'
 
   root 'home#index'
