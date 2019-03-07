65ae34cd8893895f86f5c6155a0a3f20d51df7f6
rashad <rashadmad@gmail.com>
2019-03-07 12:34:24 -0600

    make suggested changes

---
+++ vendor/cache/ruby/2.6.0/gems/dotenv-2.6.0/lib/dotenv/version.rb
@@ -0,0 +1,3 @@
+module Dotenv
+  VERSION = "2.6.0".freeze
+end
---
+++ vendor/cache/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/Rashad/Documents/Projects/giving_profiles/vendor/cache/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi/src/x86/ffitarget.h
