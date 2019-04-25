9997809e5e99a1cbf9f8b375ead06b870517cd6a
emilyspieler <38871078+emilyspieler@users.noreply.github.com>
2019-04-24 20:40:40 -0500

    policy page (#18)
    * Squashing policy page work
    
    * Use BEM for css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,7 @@ class HomeController < ApplicationController
   def about
   end
 
+  def policy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#login'
   get '/about' => 'home#about'
+  get '/policy' => 'home#policy'
 
   resources :users, only: [:show, :edit]
 end
