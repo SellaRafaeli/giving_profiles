c6fd870101971a9df4845472e4af9ef218f19e27
rashad <rashadmad@gmail.com>
2019-06-01 01:12:32 -0500

    Generate devise views.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_004858) do
+ActiveRecord::Schema.define(version: 2019_05_25_003047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2019_03_24_004858) do
     t.string "first_name"
     t.string "last_name"
     t.string "location"
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["favorite_cause"], name: "index_users_on_favorite_cause"
     t.index ["fb_id"], name: "index_users_on_fb_id", unique: true
