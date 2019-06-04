5bbdbc37f173bdfa28560a6464c0b601db8c1a9b
Gino Capio <ginocapio@gmail.com>
2019-06-04 12:38:38 -0700

    add margin below card and show user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,6 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.nick_name = auth.info.name
       user.avatar_url = auth.info.image # assuming the user model has a name
-# assuming the user model has a name
     end
   end
 
