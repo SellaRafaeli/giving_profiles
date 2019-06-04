d2ffc34f9c56709150306a1a1a68c632a1a3eccd
Gino Capio <ginocapio@gmail.com>
2019-06-04 13:19:45 -0700

    add styling to mobile home sections

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
       user.nick_name = auth.info.name
       user.avatar_url = auth.info.image # assuming the user model has a name
     end
