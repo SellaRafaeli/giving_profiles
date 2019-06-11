b44568889248351db9aebc526a276396f816cf7b
Gino Capio <ginocapio@gmail.com>
2019-06-11 13:29:10 -0700

    Get first_name, last_name, middle_name, and short_name data from fb and set them for user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,10 +24,13 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
       user.password = Devise.friendly_token[0, 20]
-      user.nick_name = auth.info.name
-      user.avatar_url = auth.info.image # assuming the user model has a name
-      # assuming the user model has a name
+      user.nick_name = auth.extra.raw_info.short_name
+      user.avatar_url = auth.info.image + "?type=large" # assuming the user model has a name
+      user.uid = auth.uid
+      user.provider = auth.provider
     end
   end
 
