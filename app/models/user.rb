93a32129e18603c71b028b8ed1068dc7ec00a756
Gino Capio <ginocapio@gmail.com>
2019-06-11 13:35:34 -0700

    Remove unused User class method, new_with_session and add validation for presence of first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,27 +12,7 @@ class User < ApplicationRecord
   has_many :donations, dependent: :destroy
 
   validates :email, uniqueness: true
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data == session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.password = Devise.friendly_token[0, 20]
-      user.nick_name = auth.extra.raw_info.short_name
-      user.avatar_url = auth.info.image + "?type=large" # assuming the user model has a name
-      user.uid = auth.uid
-      user.provider = auth.provider
-    end
-  end
+  validates_presence_of :first_name, :last_name
 
   def badges
     donated_causes.uniq
