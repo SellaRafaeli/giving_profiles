a366c26844d16af1890e2764d67bf5d88dfeefef
Gino Capio <ginocapio@gmail.com>
2019-07-03 22:45:53 -0700

    Add factory bot syntax and spec for org search

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,27 @@
 require "rails_helper"
 
 RSpec.describe Organization, type: :model do
-  it { should have_many(:donations).dependent(:destroy) }
+  it {should have_many(:donations).dependent(:destroy)}
+
+  describe "Searching" do
+    let(:organization) {create :organization}
+
+    it "return results if there are matches " do
+      name_result = PgSearch.multisearch(organization.name)
+      org_type_result = PgSearch.multisearch(organization.org_type)
+      location_result = PgSearch.multisearch(organization.location)
+
+      expect(name_result.first.searchable.name).to eq organization.name
+      expect(org_type_result.first.searchable.org_type).to eq organization.org_type
+      expect(location_result.first.searchable.location).to eq organization.location
+      expect(location_result.first.searchable.id).to eq organization.id
+
+    end
+
+    it "does NOT return results if there are no matches" do
+      result = PgSearch.multisearch("Not There")
+
+      expect(result.empty?).to be true
+    end
+  end
 end
