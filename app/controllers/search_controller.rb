b9bc4f2049d6e049f85cd638e33cbaed9ba0d25a
Gino Capio <ginocapio@gmail.com>
2019-07-05 13:18:27 -0700

    Take group by out

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,9 @@
 
 class SearchController < ApplicationController
   def index
-    search_results = PgSearch.multisearch(params[:query]).includes(:searchable).group_by(&:searchable_type)
+    search_results = PgSearch.multisearch(params[:query]).includes(:searchable)
 
-    @users = search_results["User"].first(4)
-    @organizations = search_results["Organization"].first(6)
+    @users = search_results.where(searchable_type: "User").limit(4)
+    @organizations = search_results.where(searchable_type: "Organization").limit(6)
   end
 end
