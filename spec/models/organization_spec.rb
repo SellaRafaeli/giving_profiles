feca159dc91fca50fd3dc48b49d15d603b801a73
Gino Capio <ginocapio@gmail.com>
2019-07-08 22:15:12 -0700

    Check if organization is included in results instead

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -3,32 +3,21 @@ RSpec.describe Organization, type: :model do
   it {should have_many(:donations).dependent(:destroy)}
 
   describe "Searching" do
-    before do
-      @organization =
-          Organization.create(
-              name: Faker::Company::name,
-              org_type: Organization.org_types.first.first,
-              location: "#{Faker::Address.city}, IL"
-          )
-    end
+    let(:organization) {create :organization, name: Faker::Company::name}
 
     it "return results if there are matches " do
-      name_result = PgSearch.multisearch(@organization.name)
-      org_type_result = PgSearch.multisearch(@organization.org_type)
-      location_result = PgSearch.multisearch(@organization.location)
+      name_result = PgSearch.multisearch(organization.name)
+      org_type_result = PgSearch.multisearch(organization.org_type)
+      location_result = PgSearch.multisearch(organization.location)
 
       expect(name_result.present?).to be true
-      expect(name_result.first.searchable.name).to eq @organization.name
+      expect(name_result.map(&:searchable).include?(organization)).to be true
 
       expect(org_type_result.present?).to be true
-      expect(org_type_result.first.searchable_id).to eq @organization.id
-      expect(org_type_result.first.searchable_type).to eq "Organization"
-
-      # TODO: Figure out why this is org_type_result.first.searchable.org_type is throwing undefined method `org_type' for nil:NilClass
-      # expect(org_type_result.first.searchable.org_type).to eq @organization.org_type
+      expect(org_type_result.map(&:searchable).include?(organization)).to be true
 
       expect(location_result.present?).to be true
-      expect(location_result.first.searchable.location).to eq @organization.location
+      expect(location_result.map(&:searchable).include?(organization)).to be true
     end
 
     it "does NOT return results if there are no matches" do
