0f017a87735713a262449847ca300172f09ef3d9
Mark Donald Amobi <markdamobi@gmail.com>
2019-07-09 21:25:00 -0500

    change profile flash container to BEM. add invalid changes scenario to profile edit test

--- spec/features/users/profile_edit_spec.rb
+++ spec/features/users/profile_edit_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'User profile edit' do 
+feature 'User profile edit', js: true do 
   let!(:user1) { FactoryBot.create(:user_with_fav_orgs) }
 
   scenario 'valid changes' do
@@ -12,7 +12,7 @@ feature 'User profile edit' do
     fill_in 'user_user_favorite_organizations_attributes_0_description', with: 'New favorite organization description.'
     click_button 'Save Profile'
 
-    within(".profile-flash-container") do 
+    within(".profile-flash__container") do 
       expect(page).to have_content "Successfully saved profile changes!"
     end
 
@@ -25,5 +25,16 @@ feature 'User profile edit' do
     end
 
     expect(page).to have_css(".fav-org__description", text: "New favorite organization description.")
+  end
+
+  scenario 'invalid changes' do
+    visit edit_user_path(user1)
+    fill_in 'user_first_name', with: ''
+    fill_in 'user_last_name', with: ''
+    click_button 'Save Profile'
+    within(".profile-flash__container") do 
+      expect(page).to have_content "First name can't be blank"
+      expect(page).to have_content "Last name can't be blank"
+    end
   end
 end
