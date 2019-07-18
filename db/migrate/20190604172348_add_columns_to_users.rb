03ce3fd7a0225769df3d213a00838cfb6b6e7e1a
Gino Capio <ginocapio@gmail.com>
2019-07-18 14:36:09 -0700

    Remove rename of pic_url column and remove it on a separate migration

--- db/migrate/20190604172348_add_columns_to_users.rb
+++ db/migrate/20190604172348_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :remember_created_at, :timestamp
-    rename_column :users, :pic_url, :avatar_url
+    add_column :users, :avatar_url, :string
   end
 end
