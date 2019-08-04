4e9294e02171c0109ef7c9ebc5654947e9873480
Gino Capio <ginocapio@gmail.com>
2019-08-04 15:24:16 -0700

    Add donation specs for positive value and presence for amount.
    Co-authored-by: Ricardo Maya <rickstylz01@users.noreply.github.com>

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -3,7 +3,28 @@ require "rails_helper"
 RSpec.describe Donation, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:organization) }
+
 #   should be positive amount
+  it "should have a positive amount" do
+    organization = create :organization
+    user = create :user
+    donation = Donation.new(user: user, organization: organization, amount: 1)
+
+    expect(donation).to be_valid
+
+    donation.update_attribute(:amount, -1)
+
+    expect(donation).not_to be_valid
+  end
+
+  it "should have an amount" do
+    organization = create :organization
+    user = create :user
+    donation = Donation.new(user: user, organization: organization)
+
+    expect(donation).not_to be_valid
+  end
+
 #   ordered by created_at descending
 #   DonationController
 #     - test that @new_donation is available and not null( has user)
