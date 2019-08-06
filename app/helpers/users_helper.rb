da8d3808e4697e5b47b14ad48349123083e28423
Gino Capio <ginocapio@gmail.com>
2019-08-06 16:08:25 -0700

    fix sizing of buttons and alignment
    Co-authored-by: Ricardo Maya <rickstylz01@users.noreply.github.com>

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,31 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def show_edit_button?(user)
+    if user_profile_page? && current_user_is?(user)
+      "d-block"
+    else
+      "d-none"
+    end
+  end
+
+  def show_location?
+    if user_profile_page?
+      "d-block"
+    else
+      "d-none"
+    end
+  end
+
+  def user_profile_page?
+    controller_name == "users" && action_name == "show"
+  end
+
+  def current_user_is?(user)
+    current_user == user
+  end
+
+  def user_header_avatar_size
+    user_profile_page? ? "user-header__image" : "user-header__image-xs"
+  end
 end
