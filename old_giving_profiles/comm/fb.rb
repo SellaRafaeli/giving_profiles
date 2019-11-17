4cd46b19a1cf3e61f65b52620b3a43151eb0f2b7
Paul Lorsbach <paul@lorsbach.com>
2019-11-16 19:53:29 -0600

    update http links to https

--- old_giving_profiles/comm/fb.rb
+++ old_giving_profiles/comm/fb.rb
@@ -3,7 +3,7 @@ get '/fb_login' do
   res = http_get_json "https://graph.facebook.com/me?fields=id,name,picture,email&access_token=#{pr[:token]}"
   if (id=res[:id])
     pic_url = res[:picture][:data][:url] rescue "" 
-    pic_url = "http://graph.facebook.com/#{id}/picture?type=large"
+    pic_url = "https://graph.facebook.com/#{id}/picture?type=large"
     nick    = res[:name].downcase.gsub(' ','-')
     user = $users.update_id(id,{name: res[:name], nick: nick, fb_id: id, pic_url: pic_url, deleted: false}, {upsert: true})
     session[:user_id] = user[:_id]
